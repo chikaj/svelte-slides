dcbb0e976e8d518dfd6e86f4f33cf7eefec3646b
chikaj <ncurrit@gmail.com>
2022-08-10 19:51:18 -0500

    simple slide updates

--- src/slides/Title.svelte
+++ src/slides/Title.svelte
@@ -1,34 +1,32 @@
 <script>
-    import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
-    let title;
-    onMount(() => {
-        let timeout;
-        title = new Promise(resolve => {
-            timeout = setTimeout(() => resolve('Whats nice together?'), 1000);
-        });
-        () => clearTimeout(timeout);
+  let title;
+  onMount(() => {
+    let timeout;
+    title = new Promise((resolve) => {
+      timeout = setTimeout(() => resolve("Whats nice together?"), 1000);
     });
+    () => clearTimeout(timeout);
+  });
 </script>
 
 <section>
-    <h2>
-        {#await title}
-            Wait a sec...
-        {:then result}
-            {result}
-        {/await}
-    </h2>
+  <h2>
+    {#await title}
+      Wait a sec...
+    {:then result}
+      {result}
+    {/await}
+  </h2>
 
-    <pre>
+  <pre>
     <code data-line-numbers data-trim data-no-escape>
-    {
-    `const name = "hello world";
+    {`const name = "hello world";
     if(name === 'hello') {
       console.log('world');
     }
-    `
-    }
+    `}
     </code>
     </pre>
 </section>
